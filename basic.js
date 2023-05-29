bd048583437273a315ca405f4843497fc28443e7
ryu820 <rhju820@naver.com>
2023-05-30 00:46:11 +0900

    [Day2]함수 타입 정의하기

--- basic.js
+++ basic.js
@@ -1,33 +1,20 @@
-//유니언타입
-function combine(n1, n2) {
-    //유니언 타입을 사용할 때에는 타스가 n1이 유니언타입 자체로 인식하기 때문에
-    //아래처럼 if 구문을 사용해서 정리를 해주어야 한다.
-    var result;
-    if (typeof n1 === "number" && typeof n2 === "number") {
-        result = n1 + n2;
-    }
-    else {
-        result = n1.toString() + n2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var conbinedAges = combine(30, 60);
-console.log(conbinedAges);
-var combinedName = combine("bella", "hyunju");
-console.log(combinedName);
-//리터럴타입
-//타입이 아닌 정확한 값을 가지고 있는 타입 형식이다.
-//리터럴타입으로 설정해준 값 외의 다른 값은 허용되지 않는다.
-//그렇기때문에 선언한 이후에 스펠링이 틀려서 오류가 발생하는 것을 막을 수 있다.
-function combine1(n1, n2, resultConversion) {
-    var result;
-    if (typeof n1 === "number" && typeof n2 === "number" || resultConversion === "as-number") {
-        result = +n1 + +n2;
-    }
-    else {
-        result = n1.toString() + n2.toString();
-    }
-    return result;
+function printResult(num) {
+    console.log("Result : " + num);
 }
-var combinedName1 = combine1("bella", "hyunju", "as-text");
-console.log(combinedName);
+printResult(add(5, 12));
+//함수의 타입 정의
+//함수를 정의할 변수는 앞에서 미리 선언해주는 경우 함수에 대한 타입 정의를 할 수 있다.
+//()안에는 매개변수를 => 오른쪽에는 return 값을 정의해주면 된다.
+var combineValue;
+combineValue = add;
+console.log(combineValue(8, 8));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(20, 40, function (result) {
+    console.log("콜백함수 타입 정의하기 : ", result);
+});
